56eb2d464b5935bfb310cdf9a0a71d153c4742c3
raviwhitehat <ekravi26@gmail.com>
2026-03-02 00:21:47 +0530

    Add new font assets and import configurations for Nexa and Roboto families
    - Added Nexa-ExtraLight and Nexa-Heavy font files along with their import configurations.
    - Introduced multiple Roboto font styles including Black, BlackItalic, Bold, BoldCondensed, BoldCondensedItalic, BoldItalic, Condensed, Light, Medium, Regular, Thin, and their respective italic variants.
    - Included a Roboto specimen PDF for reference.
    - Added a NOTICE.txt file for Roboto font usage permissions.

--- Scripts/Menus/pause_menu.gd
+++ Scripts/Menus/pause_menu.gd
@@ -1,13 +1,26 @@
 extends CanvasLayer
 
+func _ready() -> void:
+	visible = false
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		get_tree().paused = not get_tree().paused
-
+		if get_tree().paused:
+			visible = true
+			$AnimationPlayer.play("open")
+		else:
+			visible = false
+			$AnimationPlayer.play_backwards("open")
 
 func _on_resume_pressed() -> void:
 	get_tree().paused = not get_tree().paused
-
+	if get_tree().paused:
+		visible = true
+		$AnimationPlayer.play("open")
+	else:
+		visible = false
+		$AnimationPlayer.play_backwards("open")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit(0)
